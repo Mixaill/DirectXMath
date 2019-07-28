58720fad557927809a25431d140d4fbdd9897767
Mikhail Paulyshka <me@mixaill.tk>
2019-07-29 01:21:09 +0300

    other: add ability to build without sal.h header

--- Inc/DirectXMath.h
+++ Inc/DirectXMath.h
@@ -159,7 +159,23 @@
 #endif
 #endif // !_XM_NO_INTRINSICS_
 
+#ifdef _WIN32
 #include <sal.h>
+#else
+#define _Analysis_assume_(x)
+#define _In_
+#define _In_reads_(x)
+#define _In_reads_bytes_(x)
+#define _Inout_
+#define _Inout_updates_(x)
+#define _Out_
+#define _Out_opt_
+#define _Out_writes_(x)
+#define _Out_writes_bytes_(x)
+#define _Success_(x)
+#define _Use_decl_annotations_
+#endif
+
 #include <assert.h>
 
 #pragma warning(push)
